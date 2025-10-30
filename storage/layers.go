d281fa99c99b47934cd905cb22cc0beb969d0555
Paul Holzinger <pholzing@redhat.com>
2025-10-30 19:26:50 +0100

    storage: extract id or name conflict check into function
    Make it reusable for other callers, see next commit.
    
    Also while at it remove the dedupeStrings() call, as pointed out by
    Miloslav the work it is doing is more expensive than just checking the
    same name several times as it does a O(1) map lookup. Also most callers
    won't pass duplicated names to begin with.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- storage/layers.go
+++ storage/layers.go
@@ -1422,6 +1422,23 @@ func (r *layerStore) pickStoreLocation(volatile, writeable bool) layerLocations 
 	}
 }
 
+// checkIdOrNameConfict checks if the id or names are already in use and returns an
+// error in that case. As Special case if the layer already exists it returns it as
+// well together with the error.
+//
+// Requires startWriting.
+func (r *layerStore) checkIdOrNameConfict(id string, names []string) (*Layer, error) {
+	if duplicateLayer, idInUse := r.byid[id]; idInUse {
+		return duplicateLayer, ErrDuplicateID
+	}
+	for _, name := range names {
+		if _, nameInUse := r.byname[name]; nameInUse {
+			return nil, ErrDuplicateName
+		}
+	}
+	return nil, nil
+}
+
 // Requires startWriting.
 func (r *layerStore) create(id string, parentLayer *Layer, names []string, mountLabel string, options map[string]string, moreOptions *LayerOptions, writeable bool, slo *stagedLayerOptions) (layer *Layer, size int64, err error) {
 	if moreOptions == nil {
@@ -1444,14 +1461,8 @@ func (r *layerStore) create(id string, parentLayer *Layer, names []string, mount
 			_, idInUse = r.byid[id]
 		}
 	}
-	if duplicateLayer, idInUse := r.byid[id]; idInUse {
-		return duplicateLayer, -1, ErrDuplicateID
-	}
-	names = dedupeStrings(names)
-	for _, name := range names {
-		if _, nameInUse := r.byname[name]; nameInUse {
-			return nil, -1, ErrDuplicateName
-		}
+	if layer, err := r.checkIdOrNameConfict(id, names); err != nil {
+		return layer, -1, err
 	}
 	parent := ""
 	if parentLayer != nil {
