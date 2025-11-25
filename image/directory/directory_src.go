9cd39ec64265110ecf2451f33b4bf10b7bf1493d
Lokesh Mandvekar <lsm5@redhat.com>
2025-11-25 14:55:00 -0500

    image/directory: assign version based on digest algorithm
    Introduce version 1.2 and dynamically assign versions based on the digest
    algorithms used:
    - Version 1.1 for sha256-only images (backward compatibility)
    - Version 1.2 for images using non-sha256 digest algorithms (e.g., sha512)
    
    Add validation in both ImageDestination and ImageSource to:
    - Assume 1.1 if no version file found in dir transport images
    - Accept both version 1.1 and 1.2
    - Refuse unsupported future versions
    
    Signed-off-by: Lokesh Mandvekar <lsm5@redhat.com>

--- image/directory/directory_src.go
+++ image/directory/directory_src.go
@@ -27,6 +27,23 @@ type dirImageSource struct {
 // newImageSource returns an ImageSource reading from an existing directory.
 // The caller must call .Close() on the returned ImageSource.
 func newImageSource(ref dirReference) (private.ImageSource, error) {
+	versionPath := ref.versionPath()
+	contents, err := os.ReadFile(versionPath)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return nil, fmt.Errorf("reading version file %q: %w", versionPath, err)
+		}
+	} else {
+		versionStr := string(contents)
+		if versionStr != version && versionStr != version1_1 {
+			// Check if it's a future version we don't support
+			if versionStr > version {
+				return nil, fmt.Errorf("%w: %q", ErrUnsupportedVersion, versionStr)
+			}
+			return nil, fmt.Errorf("invalid version file content: %q", versionStr)
+		}
+	}
+
 	s := &dirImageSource{
 		PropertyMethodsInitialize: impl.PropertyMethods(impl.Properties{
 			HasThreadSafeGetBlob: false,
