03dadd20edc12713d1e36b79d9294b4dbf4c5776
Paul Holzinger <pholzing@redhat.com>
2026-01-28 16:00:57 +0100

    common: port pkg/config to new configfile package
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- common/pkg/config/config_windows.go
+++ common/pkg/config/config_windows.go
@@ -1,7 +1,5 @@
 package config
 
-import "os"
-
 const (
 	// _configPath is the path to the containers/containers.conf
 	// inside a given config directory.
@@ -17,18 +15,6 @@ const (
 	// Mount type for mounting host dir
 	_typeBind = "bind"
 )
-
-// userConfigPath returns the path to the users local config that is
-// not shared with other users. It uses $APPDATA/containers...
-func userConfigPath() (string, error) {
-	return os.Getenv("APPDATA") + _configPath, nil
-}
-
-// overrideContainersConfigPath returns the path to the system wide
-// containers config folder. It users $PROGRAMDATA/containers...
-func overrideContainersConfigPath() (string, error) {
-	return os.Getenv("ProgramData") + _configPath, nil
-}
 
 var defaultHelperBinariesDir = []string{
 	// FindHelperBinaries(), as a convention, interprets $BINDIR as the
